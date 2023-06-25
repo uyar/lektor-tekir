be0b6a84f4a1917c4ea1bc0b903dc1011dd81970
H. Turgut Uyar <uyar@tekir.org>
2023-06-25 21:47:03 +0300

    Add support for translating the UI

--- lektorly/cli.py
+++ lektorly/cli.py
@@ -3,8 +3,10 @@
 # lektorly is released under the BSD license.
 # Read the included LICENSE.txt file for details.
 
+from pathlib import Path
 from unittest.mock import patch
 
+from flask_babel import Babel, get_locale
 from lektor.admin.webui import WebUI
 from lektor.cli import cli
 
@@ -15,6 +17,10 @@ class LektorlyUI(WebUI):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.register_blueprint(dash.bp)
+        locale_dir = Path(__file__).parent / "translations"
+        _ = Babel(self, locale_selector=lambda: "tr",
+                  default_translation_directories=str(locale_dir))
+        self.jinja_env.globals["get_locale"] = get_locale
 
 
 def main():
