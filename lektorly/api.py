397ffac38bb24214688bb3c98e7a0cc97bd3da8a
H. Turgut Uyar <uyar@tekir.org>
2023-06-26 12:30:57 +0300

    Use file system path globbing to count pages

--- lektorly/api.py
+++ lektorly/api.py
@@ -3,32 +3,22 @@
 # lektorly is released under the BSD license.
 # Read the included LICENSE.txt file for details.
 
-from typing import List
+from pathlib import Path
 
 from flask import Blueprint, g
-from lektor import db
+from lektor.admin.modules.common import AdminContext
 
 
 bp = Blueprint("lektorly_api", __name__, url_prefix="/lektorly/api")
 
 
-def get_subpages(node: db.TreeItem) -> List[db.TreeItem]:
-    subpages: List[db.TreeItem] = list(node.iter_subpages())
-    descendants: List[db.TreeItem] = []
-    for subpage in subpages:
-        descendants.extend(get_subpages(subpage))
-    return subpages + descendants
-
-
-def get_subpage_count(node: db.TreeItem) -> int:
-    subpages: List[db.TreeItem] = list(node.iter_subpages())
-    n_subpages = 0
-    for subpage in subpages:
-        n_subpages += get_subpage_count(subpage)
-    return n_subpages + len(subpages)
+def get_page_count(root: Path) -> int:
+    pages = {c.parent for c in root.glob("**/contents*.lr")}
+    return len(pages)
 
 
 @bp.route("/page-count")
-def get_page_count() -> str:
-    root: db.TreeItem = g.admin_context.tree.get("/")
-    return str(get_subpage_count(root))
+def page_count() -> str:
+    context: AdminContext = g.admin_context
+    root_path = Path(context.pad.env.root_path)
+    return str(get_page_count(root_path))
