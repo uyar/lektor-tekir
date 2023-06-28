df3090f504545bc7588886bc30a092dbf079fd10
H. Turgut Uyar <uyar@tekir.org>
2023-06-28 17:07:12 +0300

    Remove mypy and tox

--- lektor_tekir_admin/api.py
+++ lektor_tekir_admin/api.py
@@ -4,10 +4,8 @@
 # Read the included LICENSE.txt file for details.
 
 from pathlib import Path
-from typing import List
 
 from flask import Blueprint, g, request, url_for
-from lektor.db import TreeItem
 
 
 LINKED_LISTITEM = """<li><a href="%(url)s">%(title)s</a></li>"""
@@ -17,37 +15,37 @@ bp = Blueprint("admin_tekir_api", __name__, url_prefix="/admin-tekir/api")
 
 
 @bp.route("/page-count")
-def page_count() -> str:
+def page_count():
     root_path = Path(g.admin_context.pad.env.root_path)
     pages = {c.parent for c in root_path.glob("**/contents*.lr")}
     return str(len(pages))
 
 
 @bp.route("/subpages")
-def subpages() -> str:
+def subpages():
     g.lang_code = request.args.get("lang", "en")
-    path: str = request.args.get("path")
-    node: TreeItem = g.admin_context.tree.get(path)
-    items: List[str] = [
+    path = request.args.get("path")
+    node = g.admin_context.tree.get(path)
+    items = [
         LINKED_LISTITEM % {
             "url": url_for("admin_tekir.contents", path=c.path),
             "title": c["title"],
         }
-        for c in [p._primary_record for p in node.iter_subpages()]
+        for c in (p._primary_record for p in node.iter_subpages())
     ]
     return "\n".join(items)
 
 
 @bp.route("/attachments")
-def attachments() -> str:
+def attachments():
     g.lang_code = request.args.get("lang", "en")
-    path: str = request.args.get("path")
-    node: TreeItem = g.admin_context.tree.get(path)
-    items: List[str] = [
+    path = request.args.get("path")
+    node = g.admin_context.tree.get(path)
+    items = [
         LINKED_LISTITEM % {
             "url": url_for("admin_tekir.contents", path=c.path),
             "title": node._primary_record.url_to(c),
         }
-        for c in [p._primary_record for p in node.iter_attachments()]
+        for c in (p._primary_record for p in node.iter_attachments())
     ]
     return "\n".join(items)
