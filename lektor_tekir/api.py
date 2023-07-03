ebd746a8bf6c08b88a55e34768d4c712efd154aa
H. Turgut Uyar <uyar@tekir.org>
2023-07-03 12:18:57 +0300

    Add support for saving flowblock fields

--- lektor_tekir/api.py
+++ lektor_tekir/api.py
@@ -9,7 +9,7 @@ from flask import Blueprint, g, render_template, request
 from flask_babel import gettext as _
 
 
-MULTILINE = {"text", "strings", "markdown", "html", "rst"}
+MULTILINE = {"text", "strings", "markdown", "html", "rst", "flow"}
 
 
 bp = Blueprint("admin_tekir_api", __name__, url_prefix="/admin-tekir/api")
@@ -42,6 +42,49 @@ def attachments():
                            current=node._primary_record, records=children)
 
 
+def field_entry(field, field_name=None):
+    if field_name is None:
+        field_name = field.name
+    value = request.form.get(field_name)
+    if field.type.name == "boolean":
+        value = "yes" if value == "on" else "no"
+    if value is not None:
+        value = value.strip()
+    if not value:
+        return None
+    if field.type.name in MULTILINE:
+        stripped = "\n".join(line.strip() for line in value.splitlines())
+        return f"{field.name}:\n\n{stripped}"
+    else:
+        return f"{field.name}: {value}"
+
+
+def flowblock_entry(node, field):
+    block_data = [k for k in request.form if k.startswith(f"{field.name}-")]
+    if len(block_data) == 0:
+        return None
+    n_blocks = max(int(k.split("-")[1]) for k in block_data)
+    entries = []
+    for i in range(1, n_blocks + 1):
+        prefix = f"{field.name}-{i}-"
+        block_fields = [k for k in request.form if k.startswith(prefix)]
+        block_types = {f.split("-")[2] for f in block_fields}
+        if len(block_types) > 1:
+            raise RuntimeError("All fields must be of the same flowblock type")
+        block_type_id = block_fields[0].split("-")[2]
+        block_header = f"#### {block_type_id} ####"
+        block_type = node.tree.pad.db.flowblocks[block_type_id]
+        block_entries = []
+        for block_field in block_type.fields:
+            field_name = f"{field.name}-{i}-{block_type_id}-{block_field.name}"
+            block_entry = field_entry(block_field, field_name)
+            if not block_entry:
+                continue
+            block_entries.append(block_entry)
+        entries.append(block_header + "\n" + "\n----\n".join(block_entries))
+    return f"{field.name}:\n\n" + "\n".join(entries)
+
+
 @bp.route("/save_content", methods=["POST"])
 def save_content():
     g.lang_code = request.args.get("lang", "en")
@@ -49,13 +92,16 @@ def save_content():
     node = g.admin_context.tree.get(path)
     entries = []
     for field in node._primary_record.datamodel.fields:
-        value = request.form.get(field.name)
-        if value is not None:
-            value = value.strip()
-            if len(value) > 0:
-                if field.type.name in MULTILINE:
-                    entries.append(f"{field.name}:\n\n{value}")
-                else:
-                    entries.append(f"{field.name}: {value}")
-    # print("\n---\n".join(entries))
+        if field.type.name == "flow":
+            entry = flowblock_entry(node, field)
+        else:
+            entry = field_entry(field)
+        if not entry:
+            continue
+        entries.append(entry)
+    content = "\n---\n".join(entries)
+    source = Path(node._primary_record.source_filename)
+    old_content = source.read_text()
+    if content != old_content:
+        source.write_text(content)
     return _("Content saved.")
