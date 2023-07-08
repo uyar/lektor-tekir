0aa35af8bd8b99ed3956cb6b80adf7f88cd56876
H. Turgut Uyar <uyar@tekir.org>
2023-07-09 02:55:17 +0300

    Store color mode preference in local storage

--- lektor_tekir/static/color-mode.js
+++ lektor_tekir/static/color-mode.js
@@ -1,10 +1,18 @@
 window.addEventListener("DOMContentLoaded", () => {
+    const colorMode = localStorage.getItem("color-mode");
+    console.log(colorMode);
+    if (colorMode) {
+        document.documentElement.setAttribute("color-mode", colorMode);
+    }
+
     document.querySelectorAll("button.color-mode").forEach(btn => {
         btn.addEventListener("click", (e) => {
             if (e.currentTarget.classList.contains("light")) {
                 document.documentElement.setAttribute("color-mode", "light");
+                localStorage.setItem("color-mode", "light");
             } else {
                 document.documentElement.setAttribute("color-mode", "dark");
+                localStorage.setItem("color-mode", "dark");
             }
         });
     });
