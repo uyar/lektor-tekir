6e7015f58d9a1f3030309145c3e224544dda769d
H. Turgut Uyar <uyar@tekir.org>
2023-07-25 13:38:57 +0300

    Group related modal actions in JS

--- lektor_tekir/static/tekir-admin.js
+++ lektor_tekir/static/tekir-admin.js
@@ -28,18 +28,15 @@ window.addEventListener("DOMContentLoaded", (loadEvent) => {
         const el = ev.target.closest("button") ?? ev.target;
         const main = el.closest("main");
 
-        if (main.classList.contains("tekir_overview")) {
-            const publishDialog = document.getElementById("publish-select");
+        if (el.classList.contains("modal-close")) {
+            ev.preventDefault();
+            el.closest("dialog").close();
+        } else if (main.classList.contains("tekir_overview")) {
             if (el.id == "tekir-publish") {
-                publishDialog.showModal();
-            } else if (el.id == "publish-cancel") {
-                ev.preventDefault();
-                publishDialog.close();
+                document.getElementById("publish-dialog").showModal();
             }
         } else if (main.classList.contains("tekir_contents")) {
-            const deleteDialog = document.getElementById("delete-confirm");
-            const addSubpageDialog = document.getElementById("add-subpage-form");
-            const addAttachmentDialog = document.getElementById("add-attachment-form");
+            const deleteDialog = document.getElementById("delete-dialog");
             if (el.classList.contains("delete-content")) {
                 document.getElementById("delete-continue").setAttribute("hx-include", "#" + el.closest("form").id);
                 deleteDialog.showModal();
@@ -49,47 +46,29 @@ window.addEventListener("DOMContentLoaded", (loadEvent) => {
                     checkbox.closest("tr").remove();
                 });
                 deleteDialog.close();
-            } else if (el.id == "delete-cancel") {
-                deleteDialog.close();
-            } else if (el.id == "add-subpage") {
+            } else if (el.classList.contains("add-content")) {
                 ev.preventDefault();
-                document.querySelector("#add-subpage-form form").reset();
-                document.getElementById("add-subpage-result").innerHTML = "";
-                addSubpageDialog.showModal();
+                const form = document.getElementById(el.dataset.formid);
+                form.reset();
+                form.querySelector(".warning").innerHTML = "";
+                form.closest("dialog").showModal();
             } else if (el.id == "add-subpage-confirm") {
                 if (document.getElementById("add-subpage-result").innerHTML == "OK") {
-                    addSubpageDialog.close();
+                    document.getElementById("add-subpage-dialog").close();
                 }
-            } else if (el.id == "add-subpage-cancel") {
-                ev.preventDefault();
-                addSubpageDialog.close();
-            } else if (el.id == "add-attachment") {
-                ev.preventDefault();
-                document.querySelector("#add-attachment-form form").reset();
-                document.getElementById("add-attachment-result").innerHTML = "";
-                addAttachmentDialog.showModal();
             } else if (el.id == "add-attachment-confirm") {
                 if (document.getElementById("add-attachment-result").innerHTML == "OK") {
-                    addAttachmentDialog.close();
+                    document.getElementById("add-attachment-dialog").close();
                 }
-            } else if (el.id == "add-attachment-cancel") {
-                ev.preventDefault();
-                addAttachmentDialog.close();
             }
         } else if (main.classList.contains("tekir_content_edit")) {
             const details = el.closest("details");
-            const saveDialog = document.getElementById("save-dialog");
-            const changesDialog = document.getElementById("changes-dialog");
             const navigateDialog = document.getElementById("navigate-dialog");
             if (el.id == "save-content") {
-                saveDialog.showModal();
-            } else if (el.id == "save-close") {
-                saveDialog.close();
+                document.getElementById("save-dialog").showModal();
             } else if (el.id == "changes-continue") {
-                changesDialog.close();
+                document.getElementById("changes-dialog").close();
                 window.location.href = el.dataset.href;
-            } else if (el.id == "changes-cancel") {
-                changesDialog.close();
             } else if (el.classList.contains("delete-block")) {
                 ev.preventDefault();
                 details.remove();
@@ -105,8 +84,6 @@ window.addEventListener("DOMContentLoaded", (loadEvent) => {
             } else if (el.id == "navigate-select") {
                 document.getElementById(el.dataset.src).value = document.getElementById("navigables").value;
                 navigateDialog.close();
-            } else if (el.id == "navigate-cancel") {
-                navigateDialog.close();
             }
         } else if (main.classList.contains("tekir_attachment_edit")) {
             const replaceAttachmentDialog = document.getElementById("replace-attachment-form");
@@ -119,16 +96,13 @@ window.addEventListener("DOMContentLoaded", (loadEvent) => {
                 if (document.getElementById("replace-attachment-result").innerHTML == "OK") {
                     replaceAttachmentDialog.close();
                 }
-            } else if (el.id == "replace-attachment-cancel") {
-                ev.preventDefault();
-                replaceAttachmentDialog.close();
             }
         }
     });
 
     document.body.addEventListener("showChanges", (ev) => {
-        document.getElementById("changes-dialog").showModal();
         document.getElementById("changes-continue").setAttribute("data-href", ev.detail.href);
+        document.getElementById("changes-dialog").showModal();
     });
 
     document.body.addEventListener("updateSlug", (ev) => {
